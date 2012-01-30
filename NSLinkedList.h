2a744a14164b24c7211cebfe9c9f3e3032f374d6
mschettler <mschettler@gmail.com>
2012-01-30 15:06:24 -0800

    fixed tabs

--- NSLinkedList.h
+++ NSLinkedList.h
@@ -5,7 +5,7 @@
 //  Created by Matt Schettler on 5/30/10.
 //  Copyright 2010 mschettler@gmail.com. All rights reserved.
 //
-//	V1.2
+//  V1.2
 //
 
 #import <Foundation/Foundation.h>
@@ -13,38 +13,38 @@
 typedef struct LNode LNode;
 
 struct LNode {
-	id obj;
-	LNode *next;
-	LNode *prev;
+    id obj;
+    LNode *next;
+    LNode *prev;
 };
 
 
 @interface NSLinkedList : NSObject {
-	
-	LNode *first;
-	LNode *last;
-	
-	unsigned int size;
-	
+    
+    LNode *first;
+    LNode *last;
+    
+    unsigned int size;
+    
 }
 
-- (id)init;									// init an empty list
-- (id)initWithObject:(id)anObject;			// init the linked list with a single object
-- (void)pushBack:(id)anObject;				// add an object to back of list
-- (void)pushFront:(id)anObject;				// add an object to front of list
-- (void)addObject:(id)anObject;				// same as pushBack
-- (id)popBack;								// remove object at end of list (returns it)
-- (id)popFront;								// remove object at front of list (returns it)
-- (void)removeNode:(LNode *)aNode;			// remove a given node
-- (BOOL)removeObjectEqualTo:(id)anObject;	// removes object equal to anObject, returns (YES) on success
-- (void)removeAllObjects;					// clear out the list
-- (void)dumpList;							// dumps all the pointers in the list to NSLog
-- (BOOL)containsObject:(id)anObject;		// (YES) if passed object is in the list, (NO) otherwise
-- (int)count;								// how many objects are stored
-- (int)size;								// how many objects are stored
-- (int)length;								// how many objects are stored
-- (void)pushNodeBack:(LNode *)n;			// adds a node object to the end of the list
-- (void)pushNodeFront:(LNode *)n;			// adds a node object to the beginning of the list
+- (id)init;                                 // init an empty list
+- (id)initWithObject:(id)anObject;          // init the linked list with a single object
+- (void)pushBack:(id)anObject;              // add an object to back of list
+- (void)pushFront:(id)anObject;             // add an object to front of list
+- (void)addObject:(id)anObject;             // same as pushBack
+- (id)popBack;                              // remove object at end of list (returns it)
+- (id)popFront;                             // remove object at front of list (returns it)
+- (void)removeNode:(LNode *)aNode;          // remove a given node
+- (BOOL)removeObjectEqualTo:(id)anObject;   // removes object equal to anObject, returns (YES) on success
+- (void)removeAllObjects;                   // clear out the list
+- (void)dumpList;                           // dumps all the pointers in the list to NSLog
+- (BOOL)containsObject:(id)anObject;        // (YES) if passed object is in the list, (NO) otherwise
+- (int)count;                               // how many objects are stored
+- (int)size;                                // how many objects are stored
+- (int)length;                              // how many objects are stored
+- (void)pushNodeBack:(LNode *)n;            // adds a node object to the end of the list
+- (void)pushNodeFront:(LNode *)n;           // adds a node object to the beginning of the list
 
 - (id)lastObject;
 - (id)secondLastObject;
@@ -59,7 +59,7 @@ struct LNode {
 - (void)prependObject:(id)anObject;
 - (void)appendObject:(id)anObject;
 
-//- (void)replaceObjectAtIndex:(int) withObject:(id)obj;	// replaces object at a given index with the passed object
+//- (void)replaceObjectAtIndex:(int) withObject:(id)obj;    // replaces object at a given index with the passed object
 
 @property (readonly) LNode *first;
 @property (readonly) LNode *last;
@@ -68,4 +68,4 @@ struct LNode {
 
 
 
-LNode * LNodeMake(id obj, LNode *next, LNode *prev);	// convenience method for creating a LNode
+LNode * LNodeMake(id obj, LNode *next, LNode *prev);    // convenience method for creating a LNode
--- NSLinkedList.m
+++ NSLinkedList.m
@@ -12,44 +12,44 @@
 @synthesize first, last;
 
 - (id)init {
-	
-	if ((self = [super init]) == nil) return nil;
-	
-	first = last = nil;
-	size = 0;
-	
-	return self;
+    
+    if ((self = [super init]) == nil) return nil;
+    
+    first = last = nil;
+    size = 0;
+    
+    return self;
 }
 
 
 - (id)initWithObject:(id)anObject {
-	
-	if ((self = [super init]) == nil) return nil;
-	
-	LNode *n = LNodeMake(anObject, nil, nil);
-	
-	first = last = n;
-	size = 1;
-	
-	return self;
+    
+    if ((self = [super init]) == nil) return nil;
+    
+    LNode *n = LNodeMake(anObject, nil, nil);
+    
+    first = last = n;
+    size = 1;
+    
+    return self;
 }
 
 
 - (void)pushBack:(id)anObject {
-	
-	if (anObject == nil) return;
-	
-	LNode *n = LNodeMake(anObject, nil, last);
-	
-	if (size == 0) {
-		first = last = n;
-	} else {
-		last->next = n;
-		last = n;
-	}
-	
-	size++;
-	
+    
+    if (anObject == nil) return;
+    
+    LNode *n = LNodeMake(anObject, nil, last);
+    
+    if (size == 0) {
+        first = last = n;
+    } else {
+        last->next = n;
+        last = n;
+    }
+    
+    size++;
+    
 }
 
 
@@ -83,20 +83,20 @@
 
 
 - (void)pushFront:(id)anObject {
-	
-	if (anObject == nil) return;
-	
-	LNode *n = LNodeMake(anObject, first, nil);
-	
-	if (size == 0) {
-		first = last = n;
-	} else {
-		first->prev = n;
-		first = n;
-	}
-	
-	size++;
-	
+    
+    if (anObject == nil) return;
+    
+    LNode *n = LNodeMake(anObject, first, nil);
+    
+    if (size == 0) {
+        first = last = n;
+    } else {
+        first->prev = n;
+        first = n;
+    }
+    
+    size++;
+    
 }
 
 
@@ -122,219 +122,219 @@
 
 - (void)insertObject:(id)anObject betweenNode:(LNode *)previousNode andNode:(LNode *)nextNode {
     
-	if (anObject == nil) return;
-	
-	LNode *n = LNodeMake(anObject, nextNode, previousNode);
-	
-	if (previousNode) {
+    if (anObject == nil) return;
+    
+    LNode *n = LNodeMake(anObject, nextNode, previousNode);
+    
+    if (previousNode) {
         previousNode->next = n;
     } else {
         first = n;
     }
     
-	if (nextNode) {
+    if (nextNode) {
         nextNode->prev = n;
     } else {
         last = n;
     }
-	
-	size++;
+    
+    size++;
     
 }
 
 
 - (void)addObject:(id)anObject {
-	[self pushBack:anObject];
+    [self pushBack:anObject];
 }
 
 
 - (void)pushNodeBack:(LNode *)n {
     
-	if (n->obj == nil) return;
-	
-	if (size == 0) {
-		first = last = n;
-	} else {
-		last->next = n;
-		last = n;
-	}
-	
-	size++;
-	
+    if (n->obj == nil) return;
+    
+    if (size == 0) {
+        first = last = n;
+    } else {
+        last->next = n;
+        last = n;
+    }
+    
+    size++;
+    
 }
 
 
 - (void)pushNodeFront:(LNode *)n {
-	
-	if (n->obj == nil) return;
     
-	if (size == 0) {
-		first = last = n;
-	} else {
-		first->prev = n;
-		first = n;
-	}
-	
-	size++;	
+    if (n->obj == nil) return;
+    
+    if (size == 0) {
+        first = last = n;
+    } else {
+        first->prev = n;
+        first = n;
+    }
+    
+    size++; 
 }
 
 
 - (id)popBack {
-	
-	if (size == 0) return nil;
-	
-	id ret = last->obj;
-	LNode *mem = last;
-	
+    
+    if (size == 0) return nil;
+    
+    id ret = last->obj;
+    LNode *mem = last;
+    
     if (size == 1) { 
         first = last = NULL;
     } else {
         last = last->prev;
         last->next = NULL;
     }
-	
-	[mem->obj release];
-	free(mem);
-	size--;
-	return ret;
-	
+    
+    [mem->obj release];
+    free(mem);
+    size--;
+    return ret;
+    
 }
 
 
 - (id)popFront {
-	
-	if (size == 0) return nil;
-	
-	id ret = first->obj;
-	LNode *mem = first;
-	
+    
+    if (size == 0) return nil;
+    
+    id ret = first->obj;
+    LNode *mem = first;
+    
     if (size == 1) { 
-		first = last = nil;
+        first = last = nil;
     } else {
         first = first->next;
         first->prev = nil;
     }
-	
-	[mem->obj release];
-	free(mem);
-	size--;
-	return ret;
-	
+    
+    [mem->obj release];
+    free(mem);
+    size--;
+    return ret;
+    
 }
 
 
 - (void)removeNode:(LNode *)aNode {
-	
-	if (size == 0) return;
-	
-	if (size == 1) {
-		// delete first and only 
-		first = last = nil;
-	} else if (aNode->prev == nil) {
-		// delete first of many
-		first = first->next;
-		first->prev = nil;
-	} else if (aNode->next == nil) {
-		// delete last
-		last = last->prev;
-		last->next = nil;
-	} else {
-		// delete in the middle
-		LNode *tmp = aNode->prev;
-		tmp->next = aNode->next;
-		tmp = aNode->next;
-		tmp->prev = aNode->prev;
-	}
-	
-	[aNode->obj release];
-	free(aNode);
-	size--;
-	
-	
+    
+    if (size == 0) return;
+    
+    if (size == 1) {
+        // delete first and only 
+        first = last = nil;
+    } else if (aNode->prev == nil) {
+        // delete first of many
+        first = first->next;
+        first->prev = nil;
+    } else if (aNode->next == nil) {
+        // delete last
+        last = last->prev;
+        last->next = nil;
+    } else {
+        // delete in the middle
+        LNode *tmp = aNode->prev;
+        tmp->next = aNode->next;
+        tmp = aNode->next;
+        tmp->prev = aNode->prev;
+    }
+    
+    [aNode->obj release];
+    free(aNode);
+    size--;
+    
+    
 }
 
 
 - (BOOL)removeObjectEqualTo:(id)anObject {
-	
+    
     LNode *n = nil;
     
-	for (n = first; n; n=n->next) {
-		if (n->obj == anObject) {
-			[n->obj release];
-			[self removeNode:n];
-			return YES;
-		}
-	}	
-	
-	return NO;
-	
+    for (n = first; n; n=n->next) {
+        if (n->obj == anObject) {
+            [n->obj release];
+            [self removeNode:n];
+            return YES;
+        }
+    }   
+    
+    return NO;
+    
 }
 
 
 - (void)removeAllObjects {
     
-	LNode *n = first;
+    LNode *n = first;
     
-	while (n) {
-		LNode *next = n->next;
-		[n->obj release];
-		free(n);
-		n = next;
-	}
+    while (n) {
+        LNode *next = n->next;
+        [n->obj release];
+        free(n);
+        n = next;
+    }
     
-	first = last = nil;
-	size = 0;
+    first = last = nil;
+    size = 0;
 }
 
 
 - (void)dumpList {
     LNode *n = nil;
-	for (n = first; n; n=n->next) {
-		NSLog(@"0x%x", (long)n);
-	}	
+    for (n = first; n; n=n->next) {
+        NSLog(@"0x%x", (long)n);
+    }   
 }
 
 
 - (int)count {
-	return size;
+    return size;
 }
 
 
 - (int)size {
-	return size;
+    return size;
 }
 
 - (int)length {
-	return size;
+    return size;
 }
 
 - (BOOL)containsObject:(id)anObject {
-	
+    
     LNode *n = nil;
     
-	for (n = first; n; n=n->next) {
-		if (n->obj == anObject) return YES;
-	}
-	
-	return NO;
-	
+    for (n = first; n; n=n->next) {
+        if (n->obj == anObject) return YES;
+    }
+    
+    return NO;
+    
 }
 
 
 - (void)dealloc {
-	[self removeAllObjects];
-	[super dealloc];
+    [self removeAllObjects];
+    [super dealloc];
 }
 
 
 @end
 
 LNode * LNodeMake(id obj, LNode *next, LNode *prev) {
-	LNode *n = malloc(sizeof(LNode));
-	n->next = next;
-	n->prev = prev;
-	n->obj = [obj retain];
-	return n;
+    LNode *n = malloc(sizeof(LNode));
+    n->next = next;
+    n->prev = prev;
+    n->obj = [obj retain];
+    return n;
 };
 
 
